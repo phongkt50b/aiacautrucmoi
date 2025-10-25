eb8bdc1daa7f11cef5bf26a197d42756c241e274
phongkt50b <nguyenphong.tfac@gmail.com>
2025-10-25 18:26:16 +0700

    Update logic.js

--- logic.js
+++ logic.js
@@ -1619,7 +1619,7 @@ function initOccupationAutocomplete(input, container) {
       autocompleteContainer.appendChild(item);
     });
     autocompleteContainer.classList.remove('hidden');
-  });
+  };
 
   input.addEventListener('input', () => {
     const value = input.value.trim().toLowerCase();
