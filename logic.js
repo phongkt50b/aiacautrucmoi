eb9cc7ad8bbe5065283ad34a7a4ae9dcc5f2868c
phongkt50b <nguyenphong.tfac@gmail.com>
2025-10-31 18:21:24 +0700

    Update logic.js

--- logic.js
+++ logic.js
@@ -1085,7 +1085,7 @@ function buildPart2ScheduleRows(ctx) {
         const suppBaseTotal = perPersonSuppBase.reduce((a, b) => a + b, 0);
         const suppAnnualEqTotal = perPersonSuppAnnualEq.reduce((a, b) => a + b, 0);
         const totalYearBase = mainYearBase + extraYearBase + suppBaseTotal;
-        const totalAnnualEq = isAnnual ? totalYearBase : (Math.round((mainYearBase + extraYearBase)/periods / 1000) * 1000)*periods + suppAnnualEqTotal;
+        const totalAnnualEq = isAnnual ? totalYearBase : (roundUpTo1000((mainYearBase + extraYearBase) / periods)) * periods + suppAnnualEqTotal;
         const diff = totalAnnualEq - totalYearBase;
         rows.push({ year, age: currentAge, mainYearBase, extraYearBase, perPersonSuppBase, perPersonSuppAnnualEq, totalYearBase, totalAnnualEq, diff });
     }
