508a8dea3432cabc224fa4f12eb3b741d38574dc
phongkt50b <nguyenphong.tfac@gmail.com>
2025-11-01 10:43:25 +0700

    Update logic.js

--- logic.js
+++ logic.js
@@ -891,7 +891,7 @@ function buildSummaryData() {
 
     allPersonsForSummary.push(...waiverOtherPersons);
 
-    const part1 = appState.context.registries.CALC_REGISTRY.buildPart1RowsData({ persons: allPersonsForSummary, productKey, paymentTerm, targetAge, riderFactor, periods, isAnnual, waiverPremiums, freq });
+    const part1 = appState.context.registries.CALC_REGISTRY.buildPart1RowsData({ persons: allPersonsForSummary, productKey, paymentTerm, targetAge, riderFactor, periods, isAnnual, waiverPremiums, freq, appState });
     const schedule = appState.context.registries.CALC_REGISTRY.buildPart2ScheduleRows({ persons: allPersonsForSummary, mainPerson, paymentTerm, targetAge, periods, isAnnual, riderFactor, productKey, waiverPremiums, appState });    
     const summary = { freq, periods, isAnnual, riderFactor, productKey, paymentTerm, targetAge, mainPerson, persons: allPersonsForSummary, waiverPremiums, part1, schedule, projection: null, sums: {} };
     
